89933c992f462e086fc2268bb241bbf8e54099e5
hoecheol <g.cheooool@gmail.com>
2020-08-27 19:41:38 +0900

    docker react example 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HelloWorld from "./HelloWorld";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld />
+      <img
+        src={
+          process.env.NODE_ENV === "development"
+            ? "https://placehold.it/100x100"
+            : "https://placehold.it/500x500"
+        }
+      />
     </div>
   );
 }
